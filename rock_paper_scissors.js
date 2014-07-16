68b61587406a7ad6eb45ef2f599b40778139bddc
johnnythecakes <1zachjohnson@gmail.com>
2014-07-15 17:54:03 -0700

    js and html now linked

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,36 +1,40 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
+function user(humanChoice) {
+    alert("you chose" + humanChoice)
+    
+    var userChoice = humanChoice;
+    var computerChoice = Math.random();
+    if (computerChoice < 0.34) {
 	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+    } else if(computerChoice <= 0.67) {
 	computerChoice = "paper";
-} else {
+    } else {
 	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
+    } alert("Computer: " + computerChoice);
 
 var compare = function(choice1,choice2) {
     if (choice1 === choice2) {
-        return "The result is a tie!";
+        alert("The result is a tie!");
     } 
     else if (choice1 === "rock") {
         if (choice2 === "scissors") {
-            return "rock wins";
+            alert("rock wins");
             } else {
-                return "paper wins";
+                alert("paper wins");
             }
         } else if (choice1 === "paper") {
             if (choice2 === "rock") {
-                return "paper wins" ;
+                alert("paper wins");
             } else {
-                return "scissors wins";
+                alert("scissors wins");
             }
         } else if (choice1 === "scissors") {
             if (choice2 === "paper") {
-                return "scissors wins" ;
+                alert("scissors wins");
             } else {
-                return "rock wins";
+                alert("rock wins");
             }
         }
     };
     
     compare (userChoice,computerChoice)
+}
